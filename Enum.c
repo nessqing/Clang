e6e9fdf2fbb3837d27b9027e6ffd5d342a30c707
peiq <88368889+nessqing@users.noreply.github.com>
2025-04-05 21:09:56 +0800

    Clean unused file
    - Remove unused file

--- Enum.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-    enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO = 10, EBAY, MICROSOFT};
-    enum Company xerox = YAHOO;
-    printf("The value of xeroxls is : %d\n", xerox);
-    return 0;
-}
--- PrintPractice.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-    float x = 3.99999999;
-    printf("%.5f",x);
-}
