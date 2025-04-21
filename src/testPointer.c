22820011007a3bc24d58aff1b1eb7944edff5e9a
peiq <88368889+nessqing@users.noreply.github.com>
2025-04-21 17:34:18 +0800

    Update variable name
    - Make it readable.

--- src/testPointer.c
+++ src/testPointer.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    int *p = &a;
+    int num = 5;
+    int *pNum = &num;
 
-    printf("a value is %d\n", a);
-    printf("&a value is %p\n", &a);
-    printf("P is %p\n", p);
-    printf("&p is %p\n", &p);
-    printf("*p is %d\n", *p);
+    printf("num value is %d\n", a);
+    printf("&num value is %p\n", &a);
+    printf("pNum is %p\n", p);
+    printf("&pNum is %p\n", &p);
+    printf("*pNum is %d\n", *p);
 
     return 0;
 }
