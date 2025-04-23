9ba6356a646273b2b7e8fb266e72c4542b93bc0e
peiq <88368889+nessqing@users.noreply.github.com>
2025-04-23 21:48:35 +0800

    Upate Pointer part
    - Add pass by reference example

--- src/testPointer.c
+++ src/testPointer.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
+//----------Test Squaring - pass by reference start----------
+void square(float *x){
+    *x = (*x) * (*x);
+}
+//----------Test Squaring - pass by reference end----------
+
+
 int main() {
+
+//----------Test Pointer start----------
     int num = 5;
-    int *pNum = &num;
+    int *pNum = &num;  
 
-    printf("num value is %d\n", a);
-    printf("&num value is %p\n", &a);
-    printf("pNum is %p\n", p);
-    printf("&pNum is %p\n", &p);
-    printf("*pNum is %d\n", *p);
+    printf("num value is %d\n", num);
+    printf("&num value is %p\n", &num);
+    printf("pNum is %p\n", pNum);
+    printf("&pNum is %p\n", &pNum);
+    printf("*pNum is %d\n", *pNum);
+//----------Test Pointer end----------
 
+
+//----------Test Squaring - pass by reference start----------
+    float x = 1.1;
+    square(&x);
+    printf("After squaring, the value of x is %.2f\n", x);
+//----------Test Squaring - pass by reference end----------
+    
     return 0;
 }
